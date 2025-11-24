5b195ae743e92f1aaa3362870884c40233b52223
2993043 <2993043@stocktonusd.org>
2025-11-24 17:44:10 +0000

    math

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+print("Welcome to the totally complicated calculator!")
+solve = input("Do you want to solve an addition or subtraction problem?(add/subtract)")
+if solve == "add" :
+    num1 = int(input("What is the 1st value?"))
+    num2 = int(input("What is the 2nd value?"))
+    sum = (num1+num2)
+    print("Your answer is: " + str(sum))
+elif solve == "subtract":
+    num1 = int(input("What is the 1st value?"))
+    num2 = int(input("What is the 2nd value?"))
+    difference = (num1-num2)
+    print("Your answer is: " + str(difference))
+else: 
+    print("Please enter either addition or subtraction!")
+
